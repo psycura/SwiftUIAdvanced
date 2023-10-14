f37f41ad568e7aaba98dc81eebe970aa6f9a7d5d
alitze2 <alitze2@medtronic.com>
2023-10-14 10:43:16 +0300

    Initial Commit

---
+++ SwiftUIAdvanced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIAdvanced
+//
+//  Created by Eugene Alitz on 14/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAdvanced/SwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedApp.swift
+//  SwiftUIAdvanced
+//
+//  Created by Eugene Alitz on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
